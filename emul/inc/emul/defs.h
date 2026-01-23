e8b99dc9753904b5cc29991cfdc64585f055ae4a
Ian Moffett <ian@lunota.org>
2026-01-23 16:19:44 -0500

    emul: Add compiler attribute macros
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- emul/inc/emul/defs.h
+++ emul/inc/emul/defs.h
@@ -6,6 +6,10 @@
 #ifndef EMUL_DEFS_H
 #define EMUL_DEFS_H 1
 
+/* Compiler attributes */
+#define PACKED  __attribute__((packed))
+
+/* Helper macros */
 #define NELEM(a) (sizeof(a) / sizeof(a[0]))
 
 #endif  /* !EMUL_DEFS_H */
