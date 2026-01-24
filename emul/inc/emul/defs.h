2b1f3ffa6f5e4e3768cfec3dd0cc010537098e8a
Ian Moffett <ian@lunota.org>
2026-01-24 09:50:38 -0500

    emul: Add extra defs
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- emul/inc/emul/defs.h
+++ emul/inc/emul/defs.h
@@ -6,10 +6,14 @@
 #ifndef EMUL_DEFS_H
 #define EMUL_DEFS_H 1
 
+/* Units */
+#define UNIT_GIB 0x40000000
+
 /* Compiler attributes */
 #define PACKED  __attribute__((packed))
 
 /* Helper macros */
 #define NELEM(a) (sizeof(a) / sizeof(a[0]))
+#define ISSET(a, b) ((a) & (b))
 
 #endif  /* !EMUL_DEFS_H */
