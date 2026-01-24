4633280cdd1904e2ff8bb886e9713c011688c58a
Ian Moffett <ian@lunota.org>
2026-01-24 01:21:15 -0500

    bios: y64a: Attempt CG line assertion + RAM test
    Signed-off-by: Ian Moffett <ian@lunota.org>

--- bios/platform/y64a/entry.asm
+++ bios/platform/y64a/entry.asm
@@ -22,6 +22,32 @@ _start:
     ;;
     mov sp, 0x110000
 
+    ;; - -TODO: Some hardware bring up needs to be done --
+
+    ;;
+    ;; Bring up external RAM by asserting the cache gate
+    ;; line through the chipset registers (@ 0x110000).
+    ;;
+    mov g0, 0x110000        ;; Chipset register base [CRB]
+    ldb g1, g0              ;; *CRB -> G1
+    or g1, 1                ;; Cache gate (CG)
+    stb g0, g1              ;; Write it back
+
+    ;;
+    ;; If the bit we wrote didn't stick, the board does not have
+    ;; external RAM. If this occurs and we are an embedded chip
+    ;; revision, we can simply rely on the 64K lcache that we are
+    ;; using now... AFTER WE FLUSH IT OF COURSE! Side-channels exist...
+    ;;
+    ;; However, if we are on a chip revision intended for personal
+    ;; computing, we should probably yield a POST failure.
+    ;;
+    ldb g1, g0      ;; *CRB -> G1
+    test g1, 1      ;; Cache gate (CG), do we have RAM?
+    bs have_ram     ;; Yes, no need to take potential action
+    hlt             ;; Halt for now (TODO: POST FAIL / INDICATE)
+have_ram:
+
     ;;
     ;; TODO: The interrupt service table needs to be placed at the bottom
     ;;       of the processor local cache
