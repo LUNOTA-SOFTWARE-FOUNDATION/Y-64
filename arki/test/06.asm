58ef0ffaa85ca0053c827ff53a292f6a4926e723
Ian Moffett <ian@lunota.org>
2026-01-25 02:15:12 -0500

    arki: test: Add test 06
    Signed-off-by: Ian Moffett <ian@lunota.org>

---
+++ arki/test/06.asm
@@ -0,0 +1,20 @@
+;;
+;; Copyright (c) 2026, Ian Moffett.
+;; Provided under the BSD-3 clause.
+;;
+
+_start:
+    mov g0, 0x100000
+    mov g1, 0xFFFF
+    stw g0, g1
+
+    mov g0, 0x110001
+    mov g1, prpd
+    stq g0, g1
+    hlt
+
+prpd:
+prpd_buf:       .byte 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00
+prpd_len:       .byte 0x10, 0x00
+prpd_chipsel:   .byte 0x00
+prpd_write:     .byte 0x01
